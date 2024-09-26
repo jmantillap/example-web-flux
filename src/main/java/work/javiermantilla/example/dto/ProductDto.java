461a4eb6d1f59fe778b2c1734cb04bdbd7c9b1ff
Javier Orlando Mantilla Portilla <jmantilla@gmail.com>
2024-09-26 13:21:28 -0500

    Se ajustan dependencias

--- src/main/java/work/javiermantilla/example/dto/ProductDto.java
+++ src/main/java/work/javiermantilla/example/dto/ProductDto.java
@@ -1,7 +1,7 @@
 package work.javiermantilla.example.dto;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
